4b5af7460a178561d70c8b8f7c937b0a9e1cf771
irobertcostin <irobertcostin@gmail.com>
2023-03-06 21:12:03 +0200

    created and rendered Car component

---
+++ src/services/Api.js
@@ -0,0 +1,54 @@
+export default class Data {
+
+    api(path, method = 'GET', body = null) {
+
+        // a function with path attribute, method (get by default) and null body
+        // null body because only reads
+
+
+        const url = "http://localhost:4444" + path;
+
+        const options = {
+
+            // GET method by default
+            method,
+
+            // header 
+            headers: {
+
+                'Content-Type': 'application/json; charset=utf-8',
+                'X-Requested-With': 'XMLHttpRequest'
+            }
+        }
+
+
+
+        // body!= null means change of state, body must receive additional info
+
+        if (body != null) {
+
+            // receive the options and transform javascript in json
+
+            options.body = JSON.stringify(body);
+
+        }
+
+        // will return the fetch
+        return fetch(url, options)
+
+
+    }
+
+
+
+
+    async getCars() {
+        let data = await this.api(`/all-cars`)
+        return data.json();
+    }
+
+
+
+
+
+}
